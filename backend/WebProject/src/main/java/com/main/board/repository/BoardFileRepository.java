bd25add3952399e93d1e031d4af64891b84e8642
joon <yijiyoon06@gmail.com>
2023-11-14 16:14:29 +0900

    Swagger 설정 파일

--- backend/WebProject/src/main/java/com/main/board/repository/BoardFileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.main.board.repository;
-
-import com.main.board.entity.BoardFileEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardFileRepository extends JpaRepository<BoardFileEntity, Long> {
-}
---
+++ backend/WebProject/src/main/java/com/main/station/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.main.station.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi api() {
+        return GroupedOpenApi.builder()
+                .group("hello")
+                .pathsToMatch("/v1/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("team_Project")
+                        .description("teamproject08 API"));
+    }
+
+}
