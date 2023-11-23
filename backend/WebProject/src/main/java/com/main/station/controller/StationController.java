80b9312be3cc836527653152b1ccfcf0d1b00c08
joon <yijiyoon06@gmail.com>
2023-11-23 13:29:28 +0900

    ApiResponses 작성

--- backend/WebProject/src/main/java/com/main/station/controller/StationController.java
+++ backend/WebProject/src/main/java/com/main/station/controller/StationController.java
@@ -4,6 +4,8 @@ import com.main.station.dto.OptimizedRoute;
 import com.main.station.dto.StationDTO;
 import com.main.station.service.RouteService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
@@ -23,12 +25,12 @@ public class StationController {
     private final RouteService routeService;
     private OptimizedRoute optimizedRoute_;
 
-    @Operation(summary = "역 정보 조회", description = "역 정보를 조회 한다.")
+    @Operation(summary = "최단 경로 검색")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "OK"),
-            @ApiResponse(responseCode = "500", description = "Internal Server Error"),
-            @ApiResponse(responseCode = "404", description = "Not found"),
-            @ApiResponse(responseCode = "400", description = "Bad Request")
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
     })
     @PostMapping("/search")
     public ResponseEntity<OptimizedRoute> search(@RequestParam("start") int start,
@@ -41,12 +43,12 @@ public class StationController {
     }
 
 
-    @Operation(summary = "최적 경로 가져오기", description = "최적의 경로를 보여준다.")
+    @Operation(summary = "최단 경로 선택")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "OK"),
-            @ApiResponse(responseCode = "500", description = "Internal Server Error"),
-            @ApiResponse(responseCode = "404", description = "Not found"),
-            @ApiResponse(responseCode = "400", description = "Bad Request")
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
     })
     @PostMapping("/select")
     public String select(@ModelAttribute OptimizedRoute optimizedRoute) throws IOException {
@@ -55,17 +57,19 @@ public class StationController {
         return "select";
     }
 
-    @Operation(summary = "최적 경로 찾기", description = "최적의 경로를 찾아 보여준다.")
+    @Operation(summary = "최단 경로 조회 결과")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "OK"),
-            @ApiResponse(responseCode = "500", description = "Internal Server Error"),
-            @ApiResponse(responseCode = "404", description = "Not found"),
-            @ApiResponse(responseCode = "400", description = "Bad Request")
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
     })
     @GetMapping("/search")
     public ResponseEntity<OptimizedRoute> getRouteData(@RequestParam int start, @RequestParam int end,  @RequestParam String type,  @RequestParam String time) throws IOException {
         optimizedRoute_ = routeService.search(start,end,type,time);
         return new ResponseEntity<>(optimizedRoute_, HttpStatus.OK);
     }
+
+
 
 }
