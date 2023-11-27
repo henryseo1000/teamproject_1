e6965a517780b0efe80904b1ccf281c812155510
서호준 <henryseo1000@gmail.com>
2023-11-27 20:14:04 +0900

    알림 보내기 테스트 성공

---
+++ frontend/app/src/main/java/com/example/mjusubwaystation_fe/activity/DetailPathActivity.java
@@ -0,0 +1,14 @@
+package com.example.mjusubwaystation_fe.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailPathActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_path);
+    }
+}
