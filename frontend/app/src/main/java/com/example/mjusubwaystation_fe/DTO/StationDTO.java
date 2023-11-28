bfb5d06a6939c72e0dd7f817f2b6d4347b907a6d
서호준 <henryseo1000@gmail.com>
2023-11-28 18:04:02 +0900

    이전역 다음역 표시 완료

--- frontend/app/src/main/java/com/example/mjusubwaystation_fe/DTO/StationDTO.java
+++ frontend/app/src/main/java/com/example/mjusubwaystation_fe/DTO/StationDTO.java
@@ -6,10 +6,22 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class StationDTO {
+    @SerializedName("nowStation")
+    private int nowStation;
+    @SerializedName("lineList")
+    private List<Integer> lineList;
     @SerializedName("surroundStationList")
     private List<List<Integer>> surroundStationList;
 
     public List<List<Integer>> getSurroundStationList() {
         return surroundStationList;
+    }
+
+    public List<Integer> getLineList() {
+        return lineList;
+    }
+
+    public int getNowStation() {
+        return nowStation;
     }
 }
